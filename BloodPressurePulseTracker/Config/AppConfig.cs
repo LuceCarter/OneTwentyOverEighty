3eb4d2c0bed9add61ee6d4c2a36e3a8024ad13ae
Luce Carter <luce@lucyscarter.co.uk>
2022-09-10 14:43:08 +0100

    Added config for app secrets

---
+++ BloodPressurePulseTracker/Config/AppConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodPressurePulseTracker.Config
+{
+    public static class AppConfig
+    {
+        public readonly static string RealmAppId = "<YOUR REALM APP ID GOES HERE>";
+    }
+}
