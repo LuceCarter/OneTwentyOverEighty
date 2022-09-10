9bbe10935dbec78f37e01b6c89129b30e5a3b5de
Luce Carter <luce@lucyscarter.co.uk>
2022-09-10 15:06:27 +0100

    Added models

---
+++ BloodPressurePulseTracker/Models/BloodPressurePulseReading.cs
@@ -0,0 +1,39 @@
+﻿x`using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodPressurePulseTracker.Models
+{
+    public class BloodPressurePulseReading : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        public ObjectId Id { get; set; } = ObjectId.GenerateNewId();
+
+        [Required]
+        [MapTo("_partition")]
+        public string PartitionKey { get; set; }
+
+        [MapTo("date")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTimeOffset Date { get; set; }
+
+        [MapTo("timeOfDay")]
+        public string TimeOfDay { get; set; }
+
+        [MapTo("systolic")]
+        public int Systolic { get; set; }
+
+        [MapTo("diastolic")]
+        public int Diastolic { get; set; }
+
+        [MapTo("pulse")]
+        public int Pulse { get; set; }
+
+        public string BloodPressure => $"{Systolic}/{Diastolic}";
+    }
+}
---
+++ BloodPressurePulseTracker/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace BloodPressurePulseTracker.Models;
+    public class User : RealmObject
+    {
+        [PrimaryKey]
+        [MapTo("_id")]
+        [Required]
+        public string Id { get; set; }
+
+        [MapTo("_partition")]
+        [Required]
+        public string Partition { get; set; }
+
+        [MapTo("name")]
+        [Required]
+        public string Name { get; set; }
+
+    }
